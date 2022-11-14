01996bc49ffc762f7bbf24f97fbcac369637e134
CondorHero <love2xinwei@gmail.com>
2022-11-14 17:57:32 +0800

    fix: online Cannot read properties of undefined (reading 'author')

--- components/AppLayout.vue
+++ components/AppLayout.vue
@@ -31,18 +31,18 @@ const formatDate = (dateStr: string) => {
     <div class="document-driven-page relative">
       <NuxtLayout :name="layout">
         <div class="absolute top-20 mt-1 text-sm text-gray-400">
-          <span class="mr-2">{{ page.author }}</span>
-          <span>{{ formatDate(page.date) }}</span>
-          <span>{{ page.duration ? `&nbsp;·&nbsp;${page.duration}` : null }}</span>
+          <span class="mr-2">{{ page?.author }}</span>
+          <span>{{ formatDate(page?.date) }}</span>
+          <span>{{ page?.duration ? `&nbsp;·&nbsp;${page?.duration}` : null }}</span>
         </div>
-        <ContentRenderer v-if="page" :key="page._id" :value="page">
+        <ContentRenderer v-if="page" :key="page?._id" :value="page">
           <template #empty="{ value }">
             <DocumentDrivenEmpty :value="value" />
           </template>
         </ContentRenderer>
         <div class="text-gray-400">
-          <span>{{page.lastmod ? `文章已于 ${formatDate(page.lastmod)} 修改 ` : null }}</span>
-          <span v-for="tag in page.tags">#{{ tag }}&nbsp;</span>
+          <span>{{page?.lastmod ? `文章已于 ${formatDate(page?.lastmod)} 修改 ` : null }}</span>
+          <span v-for="tag in page?.tags">#{{ tag }}&nbsp;</span>
         </div>
       </NuxtLayout>
     </div>
